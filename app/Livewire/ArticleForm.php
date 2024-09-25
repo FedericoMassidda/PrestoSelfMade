90f1a940b4973393993ecd44429b87b629073678
FedericoMassidda <chiccomassidda@gmail.com>
2024-09-25 12:39:25 +0200

    added real time validation

--- app/Livewire/ArticleForm.php
+++ app/Livewire/ArticleForm.php
@@ -4,19 +4,44 @@ namespace App\Livewire;
 
 use App\Models\Article;
 use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class ArticleForm extends Component
 {
-    // data binding
+    // data binding and real time validation
+    #[Validate()]
     public $title;
+    #[Validate()]
     public $price;
+    #[Validate()]
     public $description;
+    #[Validate()]
     public $category_id;
 
     // methods for article CRUD
-    // aggiungere la validazione
+    public function rules(){ //validations
+        return [
+            'title' => 'required|max:18',
+            'price' => 'required',
+            'description' => 'required|min:20|max:2000',
+            'category_id'=> 'required'
+        ];
+    }
+
+    protected $messages = [
+        'title.required' => 'Il titolo è obbligatorio.',
+        'title.max' => 'Il titolo deve avere massimo 18 caratteri.',
+        'price.required'=> 'Il prezzo è obbligatorio.',
+        'description.required'=> 'La descrizione è obbligatoria',
+        'description.min'=> 'La descrizione deve avere minimo 20 caratteri',
+        'description.max'=> 'La descrizione deve avere massimo 2000 caratteri',
+        'category_id.required'=> 'La categoria è obbligatoria'
+    ];
+
     public function store(){ //store
+        $this->validate();
+
         Article::create([
             'title'=>$this->title,
             'price'=>$this->price,
--- resources/views/livewire/article-form.blade.php
+++ resources/views/livewire/article-form.blade.php
@@ -12,25 +12,37 @@
             <div class="createArticleWrapper">
                 <div class="mb-3 createInput">
                     <label for="ArticleTitle" class="form-label">Titolo</label>
-                    <input type="text" class="form-control" id="ArticleTitle" aria-describedby="emailHelp" wire:model="title">
+                    <input type="text" class="form-control" id="ArticleTitle" aria-describedby="emailHelp" wire:model.live.debounce.500ms="title">
+                    @error('title')
+                        <p class="text-danger fst-italic">{{$message}}</p>
+                    @enderror
                 </div>
                 <div class="mb-3 createInput">
                     <label for="ArticlePrice" class="form-label">Prezzo</label>
-                    <input type="text" class="form-control" id="ArticlePrice" aria-describedby="emailHelp" wire:model="price">
+                    <input type="text" class="form-control" id="ArticlePrice" aria-describedby="emailHelp" wire:model.live.debounce.500ms="price">
+                    @error('price')
+                        <p class="text-danger fst-italic">{{$message}}</p>
+                    @enderror
                 </div>
                 <div class="createInput">
                     <label for="ArticleCategory">Categoria</label>
-                    <select class="form-select mt-1 selectInput" id="ArticleCategory" aria-label="Default select example" wire:model="category_id">
+                    <select class="form-select mt-1 selectInput" id="ArticleCategory" aria-label="Default select example" wire:model.live.debounce.500ms="category_id">
                         <option selected>Scegli la categoria</option>
                         @foreach ($categories as $category)
                             <option value="{{$category->id}}">{{$category->name}}</option>
                         @endforeach
                     </select>
+                    @error('category_id')
+                        <p class="text-danger fst-italic">{{$message}}</p>
+                    @enderror
                 </div>
             </div>
             <div class="mb-3">
                 <label for="articleDescription" class="form-label">Descrizione</label>
-                <textarea class="form-control" id="articleDescription" rows="3" wire:model="description"></textarea>
+                <textarea class="form-control" id="articleDescription" rows="3" wire:model.live.debounce.500ms="description"></textarea>
+                @error('description')
+                    <p class="text-danger fst-italic">{{$message}}</p>
+                @enderror
             </div>
             <button type="submit" class="btn btn-outline-primary mt-3 px-4 py-2">Pubblica</button>
         </form>
